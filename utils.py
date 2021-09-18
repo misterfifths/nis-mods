cec3fff825ffb8551dc1c54ab9851e8c028c40b5
Tim Clem <tim.clem@gmail.com>
2021-09-18 13:11:40 -0700

    Add ro_cached_property to utils.
    A read-only version of functools.cached_property. The docs suggest
    using @property @cache for that use case, but that was causing
    instances of ctypes things to hang out too long and cause errors
    when trying to close buffers.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any, Callable, ClassVar, Generic, Optional, TypeVar, Union
 from mmap import mmap
 import ctypes as C
 
@@ -20,3 +20,63 @@ WriteableBuffer = Union[bytearray, memoryview, mmap]
 # invented that functionality. Quoting it like this prevents the error we get
 # if we try to actually subscript it.
 AnyCType = Union['C._SimpleCData[Any]', C.Array[Any], C.Structure, C.Union]
+
+
+T = TypeVar('T')
+RT = TypeVar('RT')
+
+
+class ro_cached_property(Generic[T, RT]):
+    """A decorator for a read-only, deletable cached property.
+
+    Like cached_property but read-only and clearable.
+    Like stacked @property and @cached but lighter weight and clearable.
+
+    The decorated instance method is run only if its value is not cached. An
+    existing cached value may be cleared by deleting the property, thus
+    allowing the getter method to run again.
+
+    Values are cached as attributes on the enclosing class, so this decorator
+    can only be used on classes that allow the addition of abritrary
+    attributes.
+    """
+    CACHE_ATTR_PREFIX: ClassVar[str] = "__cached_"
+
+    func: Callable[[T], RT]
+    attr_name: Optional[str]
+
+    def __init__(self, func: Callable[[T], RT]) -> None:
+        self.func = func
+        self.attr_name = None
+        self.__doc__ = func.__doc__
+
+    def __set_name__(self, owner: Any, name: str) -> None:
+        if self.attr_name is None:
+            self.attr_name = name
+        elif name != self.attr_name:
+            raise TypeError("Cannot assign the same ro_cached_property to two different names "
+                            f"({self.attr_name!r} and {name!r}).")
+
+    def __get__(self, instance: T, owner: Any = None) -> RT:
+        if self.attr_name is None:
+            raise TypeError("Cannot use ro_cached_property instance without calling __set_name__ "
+                            "on it.")
+
+        cache_attr_name = self.CACHE_ATTR_PREFIX + self.attr_name
+        try:
+            return getattr(instance, cache_attr_name)
+        except AttributeError:
+            val = self.func(instance)
+            setattr(instance, cache_attr_name, val)
+            return val
+
+    def __delete__(self, instance: T) -> None:
+        if self.attr_name is None:
+            raise TypeError("Cannot use ro_cached_property instance without calling __set_name__ "
+                            "on it.")
+
+        cache_attr_name = self.CACHE_ATTR_PREFIX + self.attr_name
+        try:
+            delattr(instance, cache_attr_name)
+        except AttributeError:
+            pass
