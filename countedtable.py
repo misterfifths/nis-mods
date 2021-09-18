1328c9e0bb2d61f1e76e0058b908bc2d9d5c9670
Tim Clem <tim.clem@gmail.com>
2021-09-17 22:36:56 -0700

    Switch to a header struct for CountedTable rather than a factory
    Also use the AnyCType alias from utils, and a variety of fixes for
    pyright/pylance type checking.

--- countedtable.py
+++ countedtable.py
@@ -1,36 +1,31 @@
 from typing import Annotated, Sequence, TypeVar, Iterator, Union, overload
 import ctypes as C
-import struct
 from astruct import PackedAStruct, CField
-from utils import WriteableBuffer
+from utils import AnyCType, WriteableBuffer
 
 """
 TODO?
 - A MutableSequence implementation for CountedTable.
-- The E TypeVar is too narrow. It would be fine for the elements of a counted
-table to be simple ctypes like c_uint16, but the common base type (_CData) is
-not easily accessible.
-- Avoid the struct.unpack_from call, maybe? A header struct that we subclass,
-in the vein of PSPFS?
 - I'm pretty sure CountedTable.__contains__ doesn't really work, because ctypes
 Structures don't handle equality in a normal way. Probably not a big deal.
 - Is it a problem that CountedTable itself is not a Structure? The current
 approach prevents us from embedding it in other Structures, or in an array.
 """
 
-E = TypeVar('E', bound=C.Structure)
+E = TypeVar('E', bound=AnyCType)
 
 
-def structFactory(element_cls: type[E], length: int) -> type[C.Structure]:
-    """Creates and returns a ctypes.Structure subclass representing a
-    length-prefixed table of length instances of element_cls.
-    """
-    class RawCountedTable(PackedAStruct):
-        capacity: Annotated[int, CField(C.c_uint32)]
-        entry_count: Annotated[int, CField(C.c_uint32)]
-        entries: Annotated[Sequence[E], CField(element_cls * length)]
+class CountedTableHeader(PackedAStruct):
+    # TODO: unclear on the difference between these values. I assume one is
+    # capacity and the other is the actual length, but in every example I
+    # have, they're equal.
+    capacity: Annotated[int, CField(C.c_uint32)]
+    entry_count: Annotated[int, CField(C.c_uint32)]
 
-    return RawCountedTable
+    def validate(self) -> None:
+        if self.capacity != self.entry_count:
+            raise ValueError(f'Capacity ({self.capacity}) is not equal to entry count'
+                             f' ({self.entry_count})!')
 
 
 class CountedTable(Sequence[E]):
@@ -38,7 +33,8 @@ class CountedTable(Sequence[E]):
 
     This class is a sequence, and is thus iterable and indexable.
     """
-    _raw: C.Structure
+    _header: CountedTableHeader
+    _entries: C.Array[E]
 
     def __init__(self, element_cls: type[E], buffer: WriteableBuffer, offset: int = 0) -> None:
         """Create a CountedTable instance consisting of elements of type
@@ -47,30 +43,27 @@ class CountedTable(Sequence[E]):
         element_cls must be a ctypes.Structure subclass, and the buffer must be
         writeable (e.g. a bytearray, mmap, or memoryview).
         """
-        # TODO: unclear on the difference between these values. I assume one is
-        # capacity and the other is the actual length, but in every example I
-        # have, they're equal.
-        capacity, entry_count = struct.unpack_from('II', buffer, offset)
-        if capacity != entry_count:
-            raise ValueError(f'Capacity ({capacity}) is not equal to entry count ({entry_count})!')
+        self._header = CountedTableHeader.from_buffer(buffer, offset)  # type: ignore[arg-type]
+        self._header.validate()
 
-        struct_cls = structFactory(element_cls, entry_count)
-        self._raw = struct_cls.from_buffer(buffer, offset)  # type: ignore[arg-type]
+        EntriesArray = element_cls * self._header.entry_count
+        entries_offset = offset + C.sizeof(CountedTableHeader)
+        self._entries = EntriesArray.from_buffer(buffer, entries_offset)  # type: ignore[arg-type]
 
     def __iter__(self) -> Iterator[E]:
-        return iter(self._raw.entries)
+        return iter(self._entries)
 
     def __len__(self) -> int:
-        return len(self._raw.entries)
+        return len(self._entries)
 
     def __contains__(self, x: object) -> bool:
-        return x in self._raw.entries
+        return x in self._entries
 
     @overload
     def __getitem__(self, idx: int) -> E: ...
 
     @overload
-    def __getitem__(self, idx: slice) -> list[E]: ...
+    def __getitem__(self, idx: slice) -> Sequence[E]: ...
 
-    def __getitem__(self, idx: Union[slice, int]) -> Union[list[E], E]:
-        return self._raw.entries.__getitem__(idx)
+    def __getitem__(self, idx: Union[slice, int]) -> Union[Sequence[E], E]:  # type: ignore
+        return self._entries.__getitem__(idx)
