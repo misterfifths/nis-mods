a675b89c65067c8c514cb840b2530902743f8edd
Tim Clem <tim.clem@gmail.com>
2021-09-22 16:46:19 -0700

    Only import mmap in type_hints if type checking

--- astruct/type_hints/extras.py
+++ astruct/type_hints/extras.py
@@ -1,7 +1,9 @@
-from typing import Sequence, Union
+from typing import Sequence, Union, TYPE_CHECKING
 import ctypes as C
-import mmap
 from .ctypes_aliases import AnyCType
+
+if TYPE_CHECKING:
+    import mmap
 
 """
 TODO: Better WriteableBuffer type? Feels like a MutableSequence[int] should be
@@ -11,7 +13,7 @@ ctypes.Structure.from_buffer want.
 
 # Intended to represent any type that can be passed to a ctypes from_buffer
 # method, but there are some issues in the stubs that make that hard.
-WriteableBuffer = Union[bytearray, memoryview, mmap.mmap]
+WriteableBuffer = Union[bytearray, memoryview, 'mmap.mmap']
 
 # A single element of the _fields_ attribute of a ctypes.Structure or Union.
 CStructureField = Union[tuple[str, type[AnyCType]], tuple[str, type[AnyCType], int]]
